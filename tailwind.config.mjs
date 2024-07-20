8528f683a11e071edeee1c822231cc548f98c293
javier torales <jtorales2016@gmail.com>
2024-07-20 05:56:08 -0300

    dark mode add

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
 		extend: {
 			screens: {
@@ -8,6 +9,17 @@ export default {
 			  'lg': '924px',
 			  'xl': '1024px',
 			},
+			colors: {
+				'gray-1': '#718096', 
+				'gray-2': '#4A5568',
+				"principal-color":"#FFFFFF",
+				"second-color":"#F8F8FF",
+				"third-color":"#708090",
+				"dark-one":"#030712",
+				"dark-two":"#111827",
+				"dark-three":"#374151",
+			  },
+			
 		  },
 	},
 	plugins: [],

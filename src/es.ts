2e2e7c608c16706ef9c2a918b4ef05da5e3c4802
javier torales <jtorales2016@gmail.com>
2024-07-24 09:12:11 -0300

    es langague home add

---
+++ src/es.ts
@@ -0,0 +1,51 @@
+type navLink = {
+  text: string;
+  hreft: string;
+};
+export const navLinks: navLink[] = [
+  { text: "Sobre mi", hreft: "sobremi" },
+  { text: "Contacto", hreft: "contacto" },
+  { text: "Projectos", hreft: "projectos" },
+  { text: "Servicios", hreft: "servicios" },
+];
+export const home = {
+  title: "Hola, soy Javier 👋",
+  description: `Desarrollador Java Fullstack con un año y medio de experiencia construyendo aplicaciones.
+   Trabajo para descomponer lo complejo en soluciones más pequeñas que se relacionan entre sí, logrando sistemas eficientes y bien estructurados.
+`,
+  location: "Buenos Aires, Argentina",
+  available: "Disponible para realizar nuevo proyectos",
+};
+
+export const aboutMe = {
+  section: "sobre mi",
+  idSection: "sobremi",
+  title: "Curius about me? Here you have it:",
+  description: [
+    `Welcome to a little glimpse into my world! My name is Javier Torales, and
+     I’m a passionate Java Fullstack Developer with a flair for creating seamless
+     and efficient web applications. My journey in the world of programming began
+     a few years ago, and it has been a thrilling ride ever since.
+     From the moment I wrote my first line of code, I was hooked on the
+     endless possibilities that the world of software development offers. `,
+    `I recently completed a two-year technicature in programming, a period
+        during which I immersed myself in various programming languages,
+        frameworks, and tools.This experience not only solidified my technical skills but also ignited
+        a deep-seated passion for continuous learning and innovation in the tech
+        space. As a Fullstack Developer, I specialize in Java for the backend,
+        ensuring robust and scalable server-side applications.`,
+    ` For the frontend,
+        I wield React with finesse, crafting dynamic and responsive user
+        interfaces that provide a seamless user experience.
+        I thrive on the challenges that come with integrating the frontend and backend,
+        creating cohesive and efficient applications that meet user needs and exceed
+        expectations.`,
+  ],
+
+  skills: [
+    "using various technologies",
+    "using various technologies",
+    "using various technologies",
+    "using various technologies",
+  ],
+};
